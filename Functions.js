0f02afdde7aa72a14f7eac074c42b603bc49f604
Likhita <leenupandagre24@gmail.com>
2022-08-16 16:13:22 +0530

    initital commit

---
+++ Functions.js
@@ -0,0 +1,33 @@
+
+//-----------let--------------
+// let a;
+// let b;
+// demo();
+// function demo(a,b) {
+//   let add = a+b;
+//   console.log(add);
+//   return add;
+// }
+// demo(10,20);
+
+
+
+//-----------var-------------
+// var x = 3;
+// var y = 2;
+// function multiply(x,y)
+// {
+//   return x*y;
+// }
+// console.log(multiply(x,y));
+
+
+
+//----------const-------------
+// const x = 7;
+// const y = 2;
+// function multiply(x,y)
+// {
+//   return x*y;
+// }
+// console.log(multiply(x,y));
