4bf29d6c5b6c310400b2d152adddbe84a00d7482
Likhita <leenupandagre24@gmail.com>
2022-08-19 23:41:28 +0530

    Event Loop added

---
+++ LexicalScoping.js
@@ -0,0 +1,38 @@
+//Lexical scoping..
+
+// function init() {
+//     var name = 'Mozilla'; // name is a local variable created by init
+//     function displayName() {
+//       // displayName() is the inner function, a closure
+//       console.log(name); // use variable declared in the parent function
+//     }
+//     displayName();
+//   }
+//   init();
+
+
+
+
+///Scoping with var and const..
+//   if (Math.random() > 0.5) {
+//     var x = 1;
+//   } else {
+//     var x = 2;
+//   }
+//   console.log(x);
+
+
+
+///Scoping with const..
+//   if (Math.random() > 0.5) {
+//     const x = 1;
+//   } else {
+//     const x = 2;
+//   }
+//   console.log(x); // ReferenceError: x is not defined
+
+
+
+
+
+  
